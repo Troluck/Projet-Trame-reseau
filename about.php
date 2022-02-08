10234beb982603ec1479d31550378d848e94508a
Luc Boulanger <luc.boulanger98@gmail.com>
2022-02-09 00:50:15 +0100

    debut page about

---
+++ about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="about.css">
+    <title>Smart Frames- Qui sommes nous ?</title>
+</head>
+
+<body>
+    <?php include("includes/header.php") ?>
+    <main>
+        <h1> Qui sommes nous ?</h1>
+
+        <div class="carrousel">
+
+        </div>
+    </main>
+    <?php include("includes/footer.php") ?>
+</body>
+
+</html>
