dd38591a8b8956cac2f2fd8f75fcced57e6e5dbb
Luc Boulanger <luc.boulanger98@gmail.com>
2022-02-08 23:49:07 +0100

    Header+Footer fini

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,20 @@
    <footer>
        <div class="footer-left">
-           <span>Copyright © Smart Frame France</span>
+           <span>Copyright © Smart Frames France</span>
        </div>
        <div class="footer-right">
-           <a class="mention" href=#>Mentions légales |
+           <a class="mention" href=#>Mentions légales
                <span></span>
            </a>
+           <span>|</span>
            <div class="social-media">
-               <a href="#"><img src="img/facebook.png"></a>
-               <a href="#"><img src="img/twitter.png"></a>
-               <a href="#"><img src="img/linkedin.png"></a>
-               <a href="#"><img src="img/instagram.png"></a>
+               <span>Nos réseaux sociaux</span>
+               <div class="social-media-icon">
+                   <a href="#" target="_blank"><img src="img/facebook.png"></a>
+                   <a href="#" target="_blank"><img src="img/twitter.png"></a>
+                   <a href="#" target="_blank"><img src="img/linkedin.png"></a>
+                   <a href="#" target="_blank"><img src="img/instagram.png"></a>
+               </div>
            </div>
        </div>
 
