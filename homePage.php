37231d5b62a09cbc1895f5b4a5308b58f7c03c39
Luc Boulanger <luc.boulanger98@gmail.com>
2022-02-08 13:29:35 +0100

    Header + Footer

--- homePage.php
+++ homePage.php
@@ -5,23 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Smart Frames</title>
 </head>
 
 <body>
-    <header>
-        <h1 class="titleH">Smart Frames</h1>
-    </header>
+    <?php include("includes/header.php") ?>
     <main>
         <p class="text">main here</p>
         <div class="grid"></div>
     </main>
-    <footer>
-        <p class="text">footer here</p>
-    </footer>
+    <?php include("includes/footer.php") ?>
+
 </body>
 
 </html>
